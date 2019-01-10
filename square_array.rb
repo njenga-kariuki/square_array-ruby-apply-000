fd5c89d01f433ccc45bece5ace59dd86ac1707b6
Njenga Kariuki <njengak@me.com>
2019-01-10 17:04:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   # your code here
   # steps: 1. take in array, 2. square each element, 3. store squared element in new array
-  
-  array = array.each {|n| n**2}
+  array = []
+  array << array.each {|n| n**2}
   
 end
 
